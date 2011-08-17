a253282774804dd2576cfd5b82b24bd25f07d730
Julio F Schwarzbeck <julio@techfuel.net>
2011-08-16 20:38:38 -0700

    Ready 1.1.0 version

--- models/pyforum_info.py
+++ models/pyforum_info.py
@@ -1,5 +1,5 @@
 pyforum_info = {'Program Version': '1.1.0',
-                'Date Released': 'Aug 2, 2011',
+                'Date Released': 'Aug 16, 2011',
                 'Author': 'Julio Flores Schwarzbeck',
                 'Email': '<a href="mailto:julio@techfuel.net" title="">julio@techfuel.net</a>',
-                'is_beta': True}
+                'is_beta': False}
