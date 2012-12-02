e12f8347d5f2a7d186a50673a9af6cea9b00fef3
TierraDelFuego <you@example.com>
2012-12-01 18:33:38 -0800

    Some minor changes

--- models/pyforum_info.py
+++ models/pyforum_info.py
@@ -1,5 +1,5 @@
-pyforum_info = {'Program Version': '1.1.2',
-                'Date Released': 'Aug 18, 2011',
+pyforum_info = {'Program Version': '1.1.3',
+                'Date Released': 'Dec 01, 2012',
                 'Author': 'Julio Flores Schwarzbeck',
                 'Email': '<a href="mailto:julio@techfuel.net" title="">julio@techfuel.net</a>',
                 'is_beta': False}
